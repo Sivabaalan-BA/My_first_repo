15365552bfa4efeed0ffa9968ee65ffac5ed5f33
Sivabaalan.BA <vbmsbaalan@gmail.com>
2026-02-22 11:46:24 +0530

    studentname_stage3.py

--- studentname_stage3.py
+++ studentname_stage3.py
@@ -1,4 +1,26 @@
+name = input()
+mark1 = int(input())
+mark2 = int(input())
+mark3 = int(input())
 
+total = mark1 + mark2 + mark3
+percentage = (total / 300) * 100
+
+# Grade calculation
+if percentage >= 75:
+    grade = "A"
+elif percentage >= 60:
+    grade = "B"
+elif percentage >= 40:
+    grade = "C"
+else:
+    grade = "F"
+
+# Output
+print(name)
+print("Total:", total, "/300")
+print("Percentage:", percentage, "%")
+print("Grade:", grade)
 
     
     
